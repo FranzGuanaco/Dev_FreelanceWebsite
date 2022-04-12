492137e0cd446adce2c2b1e671bfe001305b150e
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-04-13 00:13:28 +0200

    seconde fois

---
+++ Port.js
@@ -0,0 +1,8 @@
+function fadein(elemntTofade) {
+    var target = document.getElementById("fade")
+    
+    element.style.opacity += 0.1;
+            if(element.style.opacity > 1.0) {
+                element.style.opacity = 1.0;
+            } else {
+      setTimeout("fadeIn(\"" + elementToFade + "\")", 100);}}
