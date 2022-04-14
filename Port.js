de726fd208356e38abdf749aa26f40df55db0dcf
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-04-14 23:09:30 +0200

    title

--- Port.js
+++ Port.js
@@ -1,8 +1,6 @@
-function fadein(elemntTofade) {
-    var target = document.getElementById("fade")
-    
-    element.style.opacity += 0.1;
-            if(element.style.opacity > 1.0) {
-                element.style.opacity = 1.0;
-            } else {
-      setTimeout("fadeIn(\"" + elementToFade + "\")", 100);}}
+$(document).ready(function(){
+
+    $("#div1").fadeIn();
+    $("#div2").fadeIn("slow");
+    $("#fade").fadeIn(5000);
+    });
