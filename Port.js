f8dcaa6a2a015e5a2b599299b9790a2866d55777
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-04-18 23:27:02 +0200

    center nav bar

--- Port.js
+++ Port.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
 
     $("#div1").fadeIn();
     $("#div2").fadeIn("slow");
-    $("#fade").fadeIn(5000);
+    $("#fade").fadeIn(4000);
+    });
+
+
+    $("#test").height($("grid-container1"))
+    $(document).ready(function() {
+        $("#rien").ready(function(){
+        $("#slide").delay(3000).slideDown("9000");})
     });
