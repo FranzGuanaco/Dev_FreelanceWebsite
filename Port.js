d8d02e70e90216807cf7e5f140620ba21ff98de2
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-04-20 00:53:07 +0200

    scroll color

--- Port.js
+++ Port.js
@@ -11,3 +11,13 @@ $(document).ready(function(){
         $("#rien").ready(function(){
         $("#slide").delay(2000).slideToggle("9000");})
     });
+
+    window.onscroll = function() {myFunction()};
+    function myFunction() {
+        if (document.documentElement.scrollTop > 44){
+            document.getElementById("changecolor").className="colorchanged";
+        }
+        else{
+            document.getElementById("changecolor").className="";
+        }
+    }
