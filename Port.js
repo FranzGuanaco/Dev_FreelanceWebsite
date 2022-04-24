508d23276a03a1ed85c61f031b5c6a50bb4793bb
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-04-24 14:44:04 +0200

    title fade

--- Port.js
+++ Port.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-
-    $("#div1").fadeIn();
-    $("#div2").fadeIn("slow");
     $("#fade").fadeIn(4000);
     });
 
@@ -12,12 +9,21 @@ $(document).ready(function(){
         $("#slide").delay(2000).slideToggle("9000");})
     });
 
+    // effet fade-in pour la couleur en background
+
     window.onscroll = function() {myFunction()};
     function myFunction() {
-        if (document.documentElement.scrollTop > 44){
+        if (document.documentElement.scrollTop > 300){
             document.getElementById("changecolor").className="colorchanged";
         }
         else{
             document.getElementById("changecolor").className="";
+        }
+    
+        if (document.documentElement.scrollTop > 50){
+            document.getElementById("fade-title").className="opacity-title";
+        }
+        else{
+            document.getElementById("fade-title").className="";
         }
     }
